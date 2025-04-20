db9e54442385402f3971377c031d8a1c39201bd4
mike7d7 <mike7d7@proton.me>
2025-04-20 10:05:16 -0600

    update vs-jetpack and jetpytools

--- vapoursynth-vs-jetpack/flake.nix
+++ vapoursynth-vs-jetpack/flake.nix
@@ -32,8 +32,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "Jaded-Encoding-Thaumaturgy";
             repo = "jetpytools";
-            rev = "691f91bda865ebc1eda92758908d538c1a6e15c8";
-            hash = "sha256-9/M+rqPNQXzErDX8KaAfv9nyUWIiTYs2F+38fN6ZvAg=";
+            rev = "83d246265bfe64b0160bc3b3e86411cab682cf0b";
+            hash = "sha256-Qx9aGBj5oxdI0KxtSJ2lgNXlkrbLTv98qgMgdXtFF/E=";
           };
         };
 
@@ -43,8 +43,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "Jaded-Encoding-Thaumaturgy";
             repo = "vs-jetpack";
-            rev = "f9aa99b18ebd0019c1d386c5868cfce3f43a1d6c";
-            hash = "sha256-Y11dexbOXIKTZnSDgF2VqEwIRMtYg7IxlWI6nDSZPcw=";
+            rev = "f2c46acd3a81c57354d67595d9446c259372ee7f";
+            hash = "sha256-OKSduSmg4x1PLA6WZ8aHlaDo0mC1K1aWxthZWNr4Ck4=";
           };
           propagatedBuildInputs = with pkgs; [
             vapoursynth
